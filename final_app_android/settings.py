86dd3171f03c17deb31b3fc73c2d3728ddcfa12d
Erdaulet0341 <erdaulet0341@gmail.com>
2023-04-10 01:13:09 +0600

    main

--- final_app_android/settings.py
+++ final_app_android/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,9 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
