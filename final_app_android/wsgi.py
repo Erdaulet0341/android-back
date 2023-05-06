c722dd40f94987391bd797a712b2c579af62c0b5
Erdaulet0341 <erdaulet0341@gmail.com>
2023-05-06 17:14:08 +0600

    update

--- final_app_android/wsgi.py
+++ final_app_android/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_app_android.settings')
 
 application = get_wsgi_application()
-
-app = application
